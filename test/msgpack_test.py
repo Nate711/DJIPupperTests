5f2e5d1fcfa25f5d7adedc60bee588dbd9f02c78
Nathan Kau <nathankau@gmail.com>
2020-09-07 21:14:35 -0700

    Test feed forward

--- test/msgpack_test.py
+++ test/msgpack_test.py
@@ -11,25 +11,28 @@ def pack_dict(dict, start=0x00):
     raw = msgpack.packb(dict, use_single_float=True)#use_bin_type=True)
     # print(raw)
     full_array = pack_serialized(raw, start)
-    print(full_array)
     return full_array
 
 with serial.Serial("/dev/tty.usbmodem78075901", timeout=0.2) as ser:
     try:
-        ser.write(pack_dict({"zero": True}))
+        # ser.write(pack_dict({"zero": False}))
         # ser.write(pack_dict({"kp": 12.0}))
         # ser.write(pack_dict({"kd": 0.005}))
-        # ser.write(pack_dict({"max_current": 0.5}))
-        ser.write(pack_dict({"cart_kp": [1.0, 2.0, 3.0]}))
-        ser.write(pack_dict({"cart_kd": [0.1, 0.2, 0.3]}))
+        ser.write(pack_dict({"max_current": 6.0}))
+        ser.write(pack_dict({"cart_kp": [0.0, 0.0, 0.0]}))
+        ser.write(pack_dict({"cart_kd": [4.0, 4.0, 4.0]}))
         ser.write(pack_dict({"activations": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}))
         # # ser.write(pack_dict({"activations": [0,0,0,0,0,0,0,0,0,0,0,0]}))
         # ser.write(
         #     pack_dict({"pos": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]})
         # )
         ser.write(
-            pack_dict({"cart_pos": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]})
+            pack_dict({"cart_pos": [0.1, -0.07, 0.0, 0.1, 0.07, 0.0, -0.1, -0.07, 0.0, -0.1, 0.07, 0.0]})
         )
+        # IN AMPS! # Rough ratio is 10A = 2.5Nm -> 0.25Nm/A.  It thinks this is N, going to Nm. So
+        # If I want weight/4 = 4.4N per leg, then command 17.6 ?????? Seems like a lot
+        #  
+        # ser.write(pack_dict({"ff_force": [0.0, 0.0, -17.6, 0.0, 0.0, -17.6, 0.0, 0.0, -17.6, 0.0, 0.0, -17.6]}))
 
         ser.flush()
         while True:
